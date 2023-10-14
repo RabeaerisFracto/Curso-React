2b984568f92da9c875bf97d073bf571f567314f3
RabeaerisFracto <flaco426@gmail.com>
2023-10-14 03:36:45 -0300

    React 1era entrada -Archivos originales -meta head html -componentes(recursos, stylesheets, jsx, img, json, fonts) -mod de App.js *prueba de carga de elementos con una sola app remplazando solo props con json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import './componentes/recursos/fonts/fonts.css'//para importar fuente, se importa css configurado con @font-face
+import dataME from './componentes/dataME.json';//importar json para evitar cambiar app
+import {Testimonio} from './componentes/testimonio';//importar componente desde js(x). si esta entre {}, es llamado x nombre.
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mass Effect</h1>{/*se pueden agregar elementos antes del componente*/}
+      <div className='contenedor-Testimonios'>
+      {dataME.map((testimonio,index)=>(/*mapeo del json, para que baje desdpues de cada ",".*/
+        <Testimonio/*nombre de complemento importado, con PascalCase*/
+        key={index}
+        nombre={testimonio.nombre}
+        profesion={testimonio.profesion}
+        avatar={testimonio.avatar}
+        descripcion={testimonio.descripcion}
+      />
+      ))}</div>
     </div>
   );
 }
 
-export default App;
+export default App;//para que se muestre en vp, debe exportarse.
---
+++ src/componentes/testimonio.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../stylesheets/Testimonio.css";
+export function Testimonio(props){
+    return (
+        <div className="contenedor-testimonio">
+            <img
+                className="avatar-testimonio"
+                src={require(`./recursos/me-${props.avatar}.jpg`)}
+                alt={props.avatar}
+                title={props.nombre}
+            />
+            <div className="contenedor-texto-testimonio">
+                <h2 className="nombre-testimonio">{props.nombre}</h2>
+                <h3 className="profesion-testimonio">{props.profesion}</h3>
+                <p className="descripcion-testimonio">"{props.descripcion}"</p>
+            </div>
+        </div>
+    );
+}
+//export default Testimonio;
