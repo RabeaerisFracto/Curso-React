98df4d68062ca0850844d96270ca5cf6faaf94fc
RabeaerisFracto <flaco426@gmail.com>
2023-10-17 02:10:03 -0300

    Vuelta a base LazyLoad se pospone limpieza de componentes de node y actualizaciones traspaso de carga json a componente Testimonio.jsx desde App.js

--- src/componentes/testimonio.jsx
+++ src/componentes/testimonio.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import "../stylesheets/Testimonio.css";
-export function Testimonio(props){
+import dataME from './dataME.json';
+
+export function Testimonio() {
     return (
-        <div className="contenedor-testimonio">
-            <img
-                className="avatar-testimonio"
-                src={require(`./recursos/img/me-${props.avatar}.jpg`)}
-                alt={props.avatar}
-                title={props.nombre}
-            />
-            <div className="contenedor-texto-testimonio">
-                <h2 className="nombre-testimonio">{props.nombre}</h2>
-                <h3 className="profesion-testimonio">{props.profesion}</h3>
-                <p className="descripcion-testimonio">"{props.descripcion}"</p>
-            </div>
+        <div>
+            {dataME.map((testimonial, index) => (
+                <div key={index} className="contenedor-testimonio">
+                    <img
+                        className="avatar-testimonio"
+                        src={require(`./recursos/img/me-${testimonial.avatar}.jpg`)}
+                        alt={testimonial.avatar}
+                        title={testimonial.nombre}
+                    />
+                    <div className="contenedor-texto-testimonio">
+                        <h2 className="nombre-testimonio">{testimonial.nombre}</h2>
+                        <h3 className="profesion-testimonio">{testimonial.profesion}</h3>
+                        <p className="descripcion-testimonio">"{testimonial.descripcion}"</p>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 }
-//export default Testimonio;
+
+export default Testimonio;
