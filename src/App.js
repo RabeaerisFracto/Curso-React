a072cb73a06ee93c58475a679eefe3bda92add5e
RabeaerisFracto <flaco426@gmail.com>
2023-10-26 23:06:45 -0300

    Axios (2da parte) -carga de elementos desde un array de arrays -NO LAZYLOAD, solo limite de elementos x cada carga actualizada. -Scroll agregado a componente pokeSearch. *Se pueden cargar mas elementos presionando un boton o con otro input.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './componentes/recursos/fonts/fonts.css'//para importar fuente, se import
 import {Testimonio} from './componentes/testimonio';//importar componente desde js(x). si esta entre {}, es llamado x nombre.
 import {Boton} from './componentes/botonContador';
 import {Contador} from './componentes/botonContador';
-import {ListaTareas} from './componentes/InputLista.tsx';
+import {ListaTareas} from './componentes/InputLista';
 import { PokeSearch } from './componentes/pokeSearch.jsx';
 import { v4 as uuidv4 } from 'uuid';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import dataME from './componentes/dataME.json';
 
 
@@ -16,14 +16,33 @@ function App() {
   const manejoClickCancelar = ()=>{setClickAmounts(0);}
   const myRef = useRef();
   
+  
+
   const[busqueda,setBusqueda] = useState('')//valores para 2 parametros
   const[numeroPagina,setnumeroPagina] = useState(1)
+  const [elementosPorPagina, setElementosPorPagina] = useState(15);
+  const cargarMasElementos = () => {
+    setElementosPorPagina(elementosPorPagina + 5)};
+
   function manejarBusqueda(e) {//funcion que da valor a busqueda
     setBusqueda(e.target.value)//lo que se va a buscar es lo que esta escrito en el input
     console.log(e.target.value)
     setnumeroPagina(1)}//cada vez que el input cambia, la pagina vuelve a uno para comenzar nueva busqueda
-
+  
   const {pokes, hayMas, carga, error} = PokeSearch(busqueda, numeroPagina)
+  // const observador = useRef()
+  // const ultimaAve = useCallback((nodo) => {
+  //   console.log(nodo)
+  //   if (nodo) {
+  //     observador.lastChild = new IntersectionObserver((entrada) => {
+  //       if (entrada.isIntersecting) {
+  //         console.log('se ve'); // Aquí se carga más elementos cuando el último elemento está intersectando
+  //       }
+  //     });
+      
+  //     observador.lastChild.observe(nodo);
+  //   }
+  // },[carga,hayMas]);
 
   return (
     <><div className="App">
@@ -39,16 +58,17 @@ function App() {
         ))}
       </div>
       <div className='contenedor-pokeSearch'>
-        <input type='text' value={busqueda} onChange={manejarBusqueda}></input> 
+        <input type='text' placeholder='busca aca' value={busqueda} onChange={manejarBusqueda}></input> 
         {/* el value va a ser busqueda, en caso que se cambie el valor en otra parte. */}
         <div>{pokes
         .filter(poke=>poke.toLowerCase().includes(busqueda))
-        .map(poke =>{
-          return <div className='bloquePoke' key={uuidv4()}>{poke}</div>
+        .slice(0, elementosPorPagina)
+        .map((poke) =>{
+          return <div  className='bloquePoke' key={uuidv4()}>{poke}</div>
           //se puede hacer un console.log para verificar los valores de "pokes". Se aplica filtro, con minusculas,
           //que incluya valor del input. Luego se mapea el resto.
         })}</div>
-        <div>{carga}</div>{/* {aparecemientras no haya nada en el input} */}
+        <div>{(!busqueda && carga) ? carga : (carga && "Cargando...")}</div>{/* {aparecemientras no haya nada en el input} */}
         <div>{error && 'error'}</div>
       </div>
       <Contador numeroClicks={clicksAmount} />
--- src/componentes/pokeSearch.jsx
+++ src/componentes/pokeSearch.jsx
@@ -14,7 +14,7 @@ export function PokeSearch(busqueda, numeroPagina) {//funcion con 2 parametros
     setCarga(true)
     setError(false)
     // let cancel//K para cancelar continua busqueda una vez hay un input escrito en app principal
-    const cancelar = new AbortController();
+    const cancelar = new AbortController();//nuevo metodo, linea anterior deprecated
     axios({//libreria actualizada de lazyLoad
       method: 'GET',//metodo, url de json o API.
       url: 'https://raw.githubusercontent.com/dariusk/corpora/master/data/animals/birds_north_america.json',
@@ -24,18 +24,16 @@ export function PokeSearch(busqueda, numeroPagina) {//funcion con 2 parametros
     }).then(res=>{//then xke devuelve una promesa
       if(!busqueda){
         setPokes([])
-      }else{setPokes([...new Set([...res.data.birds.map(b=>b.family)])])
+      }else{setPokes([...new Set(res.data.birds.flatMap(b => b.members.map(member => member.toString().split(','))).flat())]);
       //Set([]) para retornar solo valores no repetidos,
       setHayMas(res.data.birds.length > 0)//booleano para que no intente cargar mas una vez se acabe la data
       setCarga(false)//se detiene carga
-      console.log(res.data)
-      console.log(res.data.birds.map(b=>b.family))
       console.log(pokes)
     }}).catch(e=>{//catch xke retorna promesa, y es necesario en caso de error
       setError(true)
       console.log(e)//esto es para manejar el error
     })
-    cancelar.abort()
+    cancelar.abort()//nuevo metodo, junto con AbortController(), la posterior esta deprecated
     // return () => cancel()
   },[busqueda, numeroPagina])//cuando se activa funcion? cuando cambia cualquiera de estos 2 parametros
   return {carga, error, pokes, hayMas}
